42b74b2f4e42d7895bc4126b2e9b75dc8a806337
OlgitH <berman.oj@gmail.com>
2022-07-04 11:33:27 +0100

    ~

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,8 +1,46 @@
+import { useState } from "react";
+
 function Contact() {
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        setMessage(`Thank you, ${inputs.name}. An email has been sent to ${inputs.email}`);
+    }
+
+    const changeHandler = (event) => {
+        const { name, value } = event.target;
+        setInputs((state) => ({ ...state, [name]: value }));
+    };
+
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: ""
+    })
+
+    const [message, setMessage] = useState(null)
+
     return (
       <div className="Contact">
         <h1>Contact page</h1>
+
+        <form action="" onSubmit={submitHandler}>
+            <div className="form-row">
+                <label htmlFor="name">Your name:</label>
+                <input type="text" name="name" id="name" value={inputs.name} onChange={changeHandler}/>
+            </div>
+
+            <div className="form-row">
+                <label htmlFor="email">Your email:</label>
+                <input type="email" name="email" id="email" value={inputs.email} onChange={changeHandler} />
+            </div>
+
+            <button type="submit">Submit</button>
+            
+        </form>
         
+        { message && <p>{message}</p> }
+        
+
       </div>
     );
   }
